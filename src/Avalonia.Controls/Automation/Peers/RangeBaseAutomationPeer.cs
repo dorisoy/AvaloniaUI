4d52a30516b4ca42951e782966b67a5b7f230689
Steven Kirk <grokys@gmail.com>
2021-02-24 00:29:51 +0100

    Raise events when slider changed.

--- src/Avalonia.Controls/Automation/Peers/RangeBaseAutomationPeer.cs
+++ src/Avalonia.Controls/Automation/Peers/RangeBaseAutomationPeer.cs
@@ -9,11 +9,22 @@ namespace Avalonia.Controls.Automation.Peers
         public RangeBaseAutomationPeer(Control owner, AutomationRole role)
             : base(owner, role) 
         {
+            owner.PropertyChanged += OwnerPropertyChanged;
         }
 
         public double GetMaximum() => Owner.GetValue(RangeBase.MaximumProperty);
         public double GetMinimum() => Owner.GetValue(RangeBase.MinimumProperty);
         public double GetValue() => Owner.GetValue(RangeBase.ValueProperty);
         public void SetValue(double value) => Owner.SetValue(RangeBase.ValueProperty, value);
+
+        protected virtual void OwnerPropertyChanged(object sender, AvaloniaPropertyChangedEventArgs e)
+        {
+            if (e.Property == RangeBase.MinimumProperty ||
+                e.Property == RangeBase.MaximumProperty ||
+                e.Property == RangeBase.ValueProperty)
+            {
+                PlatformImpl?.PropertyChanged();
+            }
+        }
     }
 }
